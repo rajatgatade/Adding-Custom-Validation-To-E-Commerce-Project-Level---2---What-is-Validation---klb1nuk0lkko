012b57eaf9215fba4ee76890c4accc08d31e1149
rajatgatade <97111225+rajatgatade@users.noreply.github.com>
2023-04-30 20:23:03 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,19 +1,12 @@
 /* User Model */
 const mongoose = require('mongoose');
 
-/*
-Add custom validation to the User model
-Validate that the username is at least 4 characters long, doesn't contain spaces, and doesn't contain special characters
-Also add a validation for checking password length. Password should be atleast 8 chaaacters long.
-Throw an error is the Validation fails.
-*/
 const userSchema = new mongoose.Schema(
     {
         username: {
             type: String,
             required: true,
-            unique: true,
-            //Add a Custom Validation Here
+            unique: true
         },
         email: {
             type: String,
@@ -23,11 +16,9 @@ const userSchema = new mongoose.Schema(
         password: {
             type: String,
             required: true,
-            //Add a Custom Validation Here
         }
     },
     { timestamps: true }
 );
 
 module.exports = mongoose.model('User', userSchema);
-
